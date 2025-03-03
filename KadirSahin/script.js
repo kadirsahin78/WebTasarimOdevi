28b327249a04769619e471c392a82786793bd815
Kadir Şahin <kadirsahin1778@gmail.com>
2025-03-03 21:03:11 +0300

    Add files via upload

---
+++ KadirSahin/script.js
@@ -0,0 +1,28 @@
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        themeToggle.textContent = 'Light Mode';
+    } else {
+        themeToggle.textContent = 'Dark Mode';
+    }
+});
+
+const modal = document.getElementById("modal");
+        const openModal = document.getElementById("open-modal");
+        const closeButton = document.querySelector(".close-button");
+
+        openModal.addEventListener("click", () => {
+            modal.style.display = "block";
+        });
+
+        closeButton.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+
+        window.addEventListener("click", (event) => {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        });
+
